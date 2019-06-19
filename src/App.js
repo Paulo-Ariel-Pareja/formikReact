e78b434ce1c3cbd23a29e773b15a89de6fde0002
Paulo Ariel Pareja <ppareja@technisys.com>
2019-06-19 13:10:13 -0300

    formik funcionando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withFormik, Field, ErrorMessage, Form } from 'formik';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function Myform(props) {
+    const { isSubmitting, isValid } = props
+    return (
+        <Form>
+            <div className="row">
+                <Field name="email" type="email" className="input">
+                </Field>
+                <ErrorMessage name="email">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="row">
+                <Field name="password" type="password" className="input" />
+                <ErrorMessage name="password">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="row">
+                <button type="submit"
+                    disabled={isSubmitting || !isValid}
+                    className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
+                >Enviar</button>
+            </div>
+        </Form >
+    );
 }
 
-export default App;
+export default withFormik({
+    mapPropsToValues(props){
+        return {
+            email: props.defaultEmail,
+            password: '',
+        };
+    },
+    async validate(values) {
+        const errors = {};
+        if (!values.password) {
+            errors.password = 'password is required!';
+        } else if (values.password.length < 8) {
+            errors.password = 'minimo 8 caracteres';
+        }
+
+        await sleep(5000);
+
+        if(Object.keys(errors).length){
+            throw errors;
+        };
+
+    },
+    handleSubmit(values, formikBag) {
+        console.log(values);
+
+        formikBag.setSubmitting(false);
+    }
+})(Myform);
